20e0203f3df08d613376d24c8f8a33a6a48a00c2
David Masoud <davidmasoud24@gmail.com>
2022-10-12 16:02:53 -0700

    TicTacToe Final

--- app.js
+++ app.js
@@ -1,6 +1,36 @@
+const board = document.getElementById("board");
 let gameState = {
-    currentPlayer: "X";
-    board: [null, null, null, null, null, null, null, null, null],
-    
-    
-}
+  currentPlayer: "X",
+
+  board: [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null],
+  ],
+};
+const makeGameBoard = () => {
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      const cell = document.createElement("div");
+      cell.classList.add("cell");
+      cell.id = `${i},${j}`;
+      board.appendChild(cell);
+    }
+  }
+};
+makeGameBoard();
+board.addEventListener("click", (event) => {
+  playerMove(event.target.id);
+});
+
+const playerMove = (id) => {
+  console.log(id);
+  let row = id[0];
+  let column = id[2];
+  gameState.board[row][column];
+
+  if (gameState.board[row][column] === null) {
+    gameState.board[row][column] = gameState.currentPlayer;
+  }
+  console.log(gameState.board);
+};
